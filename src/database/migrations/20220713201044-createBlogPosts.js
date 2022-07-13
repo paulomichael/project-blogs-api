5b922a64e86526159295ab6092a6ce8d32bacaca
Paulo Michael <paulo.michael@gmail.com>
2022-07-13 17:31:46 -0300

    feat: req01 - creates migrations for Users, Categories, BlogPosts and PostCategories

--- src/database/migrations/20220713201044-createBlogPosts.js
+++ src/database/migrations/20220713201044-createBlogPosts.js
@@ -1,28 +1,33 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('BlogPosts', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      displayName: {
+      title: {
         type: Sequelize.STRING
       },
-      email: {
+      content: {
         type: Sequelize.STRING
       },
-      password: {
-        type: Sequelize.STRING
+      userId: {
+        type: Sequelize.INTEGER
       },
-      image: {
-        type: Sequelize.STRING
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE
       },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('BlogPosts');
   }
 };
